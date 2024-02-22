d6a011980132c5adf9cd0e1ded5e0b935e99487d
idilwor <115816447+idilwor@users.noreply.github.com>
2024-02-22 11:58:51 -0500

    yml

--- src/main/java/edu/iu/habahram/ducksservice/DucksServiceApplication.java
+++ src/main/java/edu/iu/habahram/ducksservice/DucksServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DucksServiceApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(DucksServiceApplication.class, args);
 	}
